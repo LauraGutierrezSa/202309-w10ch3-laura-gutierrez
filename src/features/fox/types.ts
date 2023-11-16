c19ff919bc6b0d4298fcdc962a9913960810cb06
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-16 15:51:08 +0100

    Add types to Fox feature

---
+++ src/features/fox/types.ts
@@ -0,0 +1,13 @@
+export interface FoxStructure {
+  _id: string;
+  name: string;
+  scientific_name: string;
+  colour: string;
+  habitat: string;
+  lifespan: number;
+}
+
+export interface FoxesRepository {
+  getFoxes: () => Promise<FoxesRepository>;
+  getFoxById: () => Promise<FoxStructure>;
+}
