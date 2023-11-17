d38bdb90a58de81c536417674c9b2fbe792c1b4e
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-17 11:30:01 +0100

    Restore accidentally deleted index file in server

---
+++ src/server/index.ts
@@ -0,0 +1,13 @@
+import morgan from "morgan";
+import app from "./app.js";
+import express from "express";
+import pingRouter from "../features/ping/router/PingRouter.js";
+import foxesRouter from "../features/fox/router/foxesRouter.js";
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/motos", foxesRouter);
+
+app.use("/", pingRouter);
