ea6caea1a260bf872cc8714ea8aa4ec525ee6b92
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-17 07:07:53 +0100

    Add mock to fox feature

---
+++ src/features/fox/mock/FoxesMock.ts
@@ -0,0 +1,26 @@
+export const foxesMock = [
+  {
+    _id: 65550,
+    name: "Red Fox",
+    scientificName: "Vulpes vulpes",
+    colour: "Reddish-brown",
+    habitat: "Forests, grasslands, and urban areas",
+    lifespan: 4,
+  },
+  {
+    _id: 65550,
+    name: "Arctic Fox",
+    scientificName: "Vulpes lagopus",
+    colour: "White in winter, brown in summer",
+    habitat: "Arctic and tundra regions",
+    lifespan: 6,
+  },
+  {
+    _id: 65550,
+    name: "Fennec Fox",
+    scientificName: "Vulpes zerda",
+    colour: "Pale yellow or cream",
+    habitat: "Deserts and arid regions",
+    lifespan: 14,
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import morgan from "morgan";
 import pingRouter from "./features/ping/router/PingRouter.js";
 import { connectToDatabase } from "./database/index.js";
 import express from "express";
+import foxesRouter from "./features/fox/router/foxesRouter.js";
 
 app.use(express.json());
 app.use(morgan("dev"));
-
 app.use("/", pingRouter);
+app.use("/foxes", foxesRouter);
 
 const mongoUrl = process.env.MONGODB_URL;
 
